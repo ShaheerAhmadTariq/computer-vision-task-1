ed846d39e7f44ebe20ee12cbf1c5f24505c48ce1
shaheerahmadtariq <58523793+shaheerahmadtariq@users.noreply.github.com>
2022-09-26 14:29:08 +0500

    Proejct deployment

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express= require("express")
+const cors=require("cors")
+const morgan=require("morgan")
+const path=require("path")
+require("dotenv").config({path:"./.env.local"})
+
+const app=express()
+if(process.env.NODE_ENV==="production"){
+app.use(express.static(path.join(__dirname,"..","client","build")))
+
+app.use("*",(req,res)=>{
+res.sendFile(path.join(__dirname,"..","client","build","index.html"))
+})
+}
+app.listen(3001,()=>console.log("Listening on port 3001"))
