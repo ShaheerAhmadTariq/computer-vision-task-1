6c8e96612f315f8b33663617d5d131824de8fa98
shaheerahmadtariq <58523793+shaheerahmadtariq@users.noreply.github.com>
2022-09-26 16:32:45 +0500

    Port changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors=require("cors")
 const morgan=require("morgan")
 const path=require("path")
 // require("dotenv").config({path:"./.env.local"})
-
+const port=3001||process.env.PORT
 const app=express()
 if(process.env.NODE_ENV==="production"){
 app.use(express.static(path.join(__dirname,"client","build")))
@@ -12,4 +12,4 @@ app.use("*",(req,res)=>{
 res.sendFile(path.resolve(__dirname,"client","build","index.html"))
 })
 }
-app.listen(3001,()=>console.log("Listening on port 3001"))
+app.listen(port,()=>console.log("Listening on port 3001"))
